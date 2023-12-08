2a2ad9e46a691a1ad2b4e2c32a5e29dea1755850
jackie-h <10471950+jackie-h@users.noreply.github.com>
2023-12-08 12:33:36 -0500

    day 8 part two naive too slow

--- day8.py
+++ day8.py
@@ -1,17 +1,18 @@
 
 
-def day8(filename):
+def day8(filename, part2):
     print('Day 8: Haunted Wasteland')
 
     res = 0
     with open(filename) as f:
         lines = [line.strip() for line in f.readlines()]
-        res = solve(lines)
+        res = solve(lines, part2)
         print(res)
 
     return res
 
-def solve(lines):
+
+def solve(lines, part2):
     instruction = lines[0]
     map = {}
 
@@ -25,10 +26,33 @@ def solve(lines):
             map[key] = (lv,rv)
     print(map)
 
-    key = 'AAA'
     steps = 0
-    while key != 'ZZZ':
-        key,steps = follow_path(instruction, key, map, steps)
+    if not part2:
+        key = 'AAA'
+        while key != 'ZZZ':
+            key,steps = follow_path(instruction, key, map, steps)
+    else:
+        # Part 2
+        keys = [k for k in map.keys() if k.endswith('A')]
+        print(keys)
+        more = True
+
+        while more:
+            for s in instruction:
+                steps += 1
+                for i,key in enumerate(keys):
+                    key = follow_step(s, key, map)
+                    keys[i] = key
+
+                print(steps, keys)
+                stop = True
+                for k in keys:
+                    if not k.endswith('Z'):
+                        stop = False
+                        break
+                if stop:
+                    more = False
+                    break
 
     return steps
 
@@ -36,16 +60,25 @@ def solve(lines):
 def follow_path(instruction, key, map, steps):
     for s in instruction:
         steps += 1
-        l, r = map.get(key)
-        if s == 'L':
-            key = l
-        elif s == 'R':
-            key = r
+        key = follow_step(s, key, map)
         if key == 'ZZZ':
             break
     return key,steps
 
+
+def follow_step(s, key, map):
+    l, r = map.get(key)
+    if s == 'L':
+        key = l
+    elif s == 'R':
+        key = r
+    return key
+
+
 if __name__ == '__main__':
-    assert day8('day8_test1.txt') == 2
-    assert day8('day8_test2.txt') == 6
-    assert day8('day8_input.txt') == 12737
+    #assert day8('day8_test1.txt', False) == 2
+    #assert day8('day8_test2.txt', False) == 6
+    #assert day8('day8_test3.txt', True) == 6
+
+    #assert day8('day8_input.txt', False) == 12737
+    assert day8('day8_input.txt', True) == 12737
