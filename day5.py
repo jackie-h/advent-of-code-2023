3fa339e336b504bd1624704794410f2d1cc08428
jackie-h <10471950+jackie-h@users.noreply.github.com>
2023-12-09 18:36:14 -0500

    day 5 still trying

--- day5.py
+++ day5.py
@@ -1,4 +1,3 @@
-
 def day5(filename):
     print('Day 5: If You Give A Seed A Fertilizer')
 
@@ -20,6 +19,7 @@ def solve(lines):
     for line in lines:
         if line.startswith("seeds:"):
             seeds = line.split(":")[1].split()
+            seeds = list(map(int, seeds))
         elif ':' in line:
             map_name = line.split(':')[0]
             s_maps[map_name] = []
@@ -27,81 +27,124 @@ def solve(lines):
             numbers = line.strip().split()
             if len(numbers) > 0:
                 print(numbers)
-                s_maps.get(map_name).append(numbers)
+                s_maps.get(map_name).append(list(map(int, numbers)))
 
     print('seeds', seeds)
     print('maps', s_maps)
 
     for seed in seeds:
-        soil = get_value(int(seed), s_maps.get('seed-to-soil map'))
+        soil = get_value(seed, s_maps.get('seed-to-soil map'))
         fertilizer = get_value(soil, s_maps.get('soil-to-fertilizer map'))
         water = get_value(fertilizer, s_maps.get('fertilizer-to-water map'))
         light = get_value(water, s_maps.get('water-to-light map'))
         temp = get_value(light, s_maps.get('light-to-temperature map'))
         humidity = get_value(temp, s_maps.get('temperature-to-humidity map'))
         location = get_value(humidity, s_maps.get('humidity-to-location map'))
-        print(soil,fertilizer,water,light,temp,humidity)
+        print(soil, fertilizer, water, light, temp, humidity)
         print('location', location)
         if res is None:
             res = location
         else:
-            res = min(res,location)
+            res = min(res, location)
 
+    print('Part 2')
     i = 0
     while i < len(seeds):
         seed_start = seeds[i]
-        seed_range = seeds[i+1]
-        i = i+2
-        soil = get_value_ranges([[seed_start,seed_range]], s_maps.get('seed-to-soil map'))
+        seed_range = seeds[i + 1]
+        i = i + 2
+        print('seed=', [seed_start, seed_range])
+        soil = get_value_ranges([[seed_start, seed_range]], s_maps.get('seed-to-soil map'))
+        print('soil=', soil)
         fertilizer = get_value_ranges(soil, s_maps.get('soil-to-fertilizer map'))
+        print('fertilizer=', fertilizer)
         water = get_value_ranges(fertilizer, s_maps.get('fertilizer-to-water map'))
+        print('water=', water)
         light = get_value_ranges(water, s_maps.get('water-to-light map'))
+        print('light=', light)
         temp = get_value_ranges(light, s_maps.get('light-to-temperature map'))
+        print('temp=', temp)
         humidity = get_value_ranges(temp, s_maps.get('temperature-to-humidity map'))
+        print('humidity=', humidity)
         locations = get_value_ranges(humidity, s_maps.get('humidity-to-location map'))
-        print(soil,fertilizer,water,light,temp,humidity)
         print('location', locations)
         for lr in locations:
             if res2 is None:
                 res2 = int(lr[0])
             else:
-                res2 = min(res2,int(lr[0]))
+                res2 = min(res2, int(lr[0]))
 
-    return res,res2
+    return res, res2
+
 
 def get_value_ranges(ranges, values):
     valid_ranges = []
-    for range in ranges:
-        start = int(range[0])
-        incr = int(range[1])
+    intersected = []
+
+    min_start = None
+    for rg in ranges:
+        start = rg[0]
+        incr = rg[1]
         end = start + incr
         for row in values:
-            source_start = int(row[1])
-            rl = int(row[2])
+            source_start = row[1]
+            rl = row[2]
             source_end = source_start + rl
-            dest_start = int(row[0])
-            if (start < source_start and end > source_end):
-                valid_ranges.append([dest_start, rl])
-            elif (start >= source_start and start <= source_end):
-                delta = int(start) - source_start
-                valid_ranges.append([dest_start + delta, rl - delta])
-            elif (end >= source_start and end <= source_end):
-                valid_ranges.append([dest_start,min(end,source_end) - source_start])
+            intersect = range_intersect(range(start, end), range(source_start, source_end))
+            if min_start is None:
+                min_start = start
+            else:
+                min_start = min(min_start, start)
+            if intersect is not None:
+                intersected.append(intersect)
+                dest_start = row[0]
+                s_offset = intersect.start - source_start
+                ds = dest_start + s_offset
+                r = intersect.stop - intersect.start
+                valid_ranges.append([ds, r])
+                # if start < intersect.start:
+                #     valid_ranges.append([start,intersect.start - start])
+                # if end > intersect.stop:
+                #     valid_ranges.append([intersect.stop+1, end - intersect.stop])
+        # remaining = [(start,end)]
+        # for inter in intersected:
+        #
+        #     x = remaining[0] - inter
+
+
+    if len(valid_ranges) == 0:
+        valid_ranges = ranges
+    else:
+        min_inter_s = None
+
+        for inter in intersected:
+            if min_inter_s is None:
+                min_inter_s = inter.start
+            else:
+                min_inter_s = min(min_inter_s,inter.start)
+        if min_start < min_inter_s:
+            valid_ranges.append([min_start, min_inter_s - min_start])
+    print('intersected=', intersected)
     return valid_ranges
+
+
+def range_intersect(r1, r2):
+    return (range(max(r1.start,r2.start), min(r1.stop,r2.stop))) or None
+
 
 def get_value(input, values):
     for row in values:
-        source_start = int(row[1])
+        source_start = row[1]
         if input >= source_start:
-            rl = int(row[2])
+            rl = row[2]
             source_end = source_start + rl
             if input <= source_end:
-                dest_start = int(row[0])
+                dest_start = row[0]
                 return dest_start + (input - source_start)
     return input
 
+
 if __name__ == '__main__':
-    assert day5('day5_test.txt') == (35,46)
+    assert day5('day5_test.txt') == (35, 46)
 
     assert day5('day5_input.txt') == 403695602
-
