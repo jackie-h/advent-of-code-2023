cd8863cf52530542bb75d8565614687413685a66
jackie-h <10471950+jackie-h@users.noreply.github.com>
2023-12-18 14:47:00 +0000

    day 18 part 2

--- day18.py
+++ day18.py
@@ -14,41 +14,51 @@ def day18(filename, part2):
 def solve(lines, part2):
     res = 0
 
-    if not part2:
-        cx, cy = 0,0
-        border_len = 1
-        coords = [(cx, cy)]
-        for line in lines:
-            d,sv,c = line.split()
-            v = int(sv)
-            ix = 0
-            iy = 0
-            if d == 'R':
-                iy = 1
-            elif d == 'L':
-                iy = -1
-            elif d == 'D':
-                ix = 1
-            elif d == 'U':
-                ix = -1
+    cx, cy = 0,0
+    border_len = 1
+    coords = [(cx, cy)]
+    for line in lines:
+        d,sv,c = line.split()
+        v = int(sv)
 
-            cx = cx + ix * v
-            cy = cy + iy * v
-            border_len += v
-            coords.append((cx,cy))
+        if part2:
+            hex_s = c[2:7]
+            sd = c[7:8]
+            v = int(hex_s, 16)
+            if sd == '0':
+                d = 'R'
+            elif sd == '1':
+                d = 'D'
+            elif sd == '2':
+                d = 'L'
+            elif sd == '3':
+                d = 'U'
 
-        # area
-        area = 0
-        for i in range(1, len(coords)):
-            x1, y1 = coords[i - 1]
-            x2, y2 = coords[i]
-            area += (y1 + y2) * (x1 - x2)
+        ix = 0
+        iy = 0
+        if d == 'R':
+            iy = 1
+        elif d == 'L':
+            iy = -1
+        elif d == 'D':
+            ix = 1
+        elif d == 'U':
+            ix = -1
 
-        area = (abs(area) + border_len + 1) / 2
-        res = area
+        cx = cx + ix * v
+        cy = cy + iy * v
+        border_len += v
+        coords.append((cx,cy))
 
-    else:
-        print('')
+    # area
+    area = 0
+    for i in range(1, len(coords)):
+        x1, y1 = coords[i - 1]
+        x2, y2 = coords[i]
+        area += (y1 + y2) * (x1 - x2)
+
+    area = (abs(area) + border_len + 1) / 2
+    res = area
 
     return res
 
@@ -57,6 +67,9 @@ if __name__ == '__main__':
     assert day18('day18_test.txt', False) == 62
     assert day18('day18_test2.txt', False) == 33
     assert day18('day18_input.txt', False) == 70026
+
+    assert day18('day18_test.txt', True) == 952408144115
+    assert day18('day18_input.txt', True) == 70026
 
 
 
