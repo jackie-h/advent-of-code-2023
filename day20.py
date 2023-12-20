6820f9ede3a754595578a29d21dc5c67913a5dbe
jackie-h <10471950+jackie-h@users.noreply.github.com>
2023-12-20 19:16:08 +0000

    day 20 part 2 initial

--- day20.py
+++ day20.py
@@ -1,4 +1,3 @@
-
 def day20(filename, part2):
     print('Day 20: Pulse Propagation')
 
@@ -22,39 +21,56 @@ def solve(lines, part2):
             ptype = name[0:1]
             name = name[1:]
         conns = tail.split(', ')
-        connections[name] = (ptype,conns)
+        connections[name] = (ptype, conns)
 
     states = {}
 
-    total = [0,0]
+    total = [0, 0]
 
-    for i in range(0,1000):
-        pulses = push(connections,states)
-        print(i,pulses,res)
-        total[0] = total[0] + pulses[0]
-        total[1] = total[1] + pulses[1]
+    i = 0
+    rx = False
+    if part2:
+        while not rx:
+            i += 1
+            print(i)
+            rx, pulses = push(connections, states, part2)
 
-    res = total[0] * total[1]
-    return res
+    else:
+        for i in range(0, 1000):
+            rx, pulses = push(connections, states, part2)
+            print(i, pulses, res)
+            total[0] = total[0] + pulses[0]
+            total[1] = total[1] + pulses[1]
+        res = total[0] * total[1]
+
+    if part2:
+        return i
+    else:
+        return res
 
 
-def push(connections, state):
-    next:list = connections['broadcaster'][1]
+def push(connections, state, part2):
+    next: list = connections['broadcaster'][1]
     pulse = 0
     prev = 'broadcaster'
-    pulses = [1,0]
-    pulses = move(connections, [(prev,pulse,next)], pulses, state)
+    pulses = [1, 0]
+    rx, pulses = move(connections, [(prev, pulse, next)], pulses, state, part2)
 
-    return pulses
+    return rx, pulses
 
 
-def move(connections, move_blocks, pulses, state):
+def move(connections, move_blocks, pulses, state, part2):
     next = []
-
-    for prev,pulse,moves in move_blocks:
+    rx = False
+    for prev, pulse, moves in move_blocks:
         for n in moves:
-            print(prev, pulse, '->', n)
+            #print(prev, pulse, '->', n)
             pulses[pulse] += 1
+            if part2 and n == 'rx':
+                if pulse == 0:
+                    rx = True
+                    break
+
             if connections.get(n) is not None:
                 type, x = connections[n]
                 if type == '%':
@@ -83,12 +99,13 @@ def move(connections, move_blocks, pulses, state):
                     else:
                         next.append((n, 1, x))
 
-    if len(next) > 0:
-        pulses = move(connections, next, pulses, state)
-    return pulses
+    if len(next) > 0 and not rx:
+        rx, pulses = move(connections, next, pulses, state, part2)
+    return rx, pulses
 
 
 if __name__ == '__main__':
     assert day20('day20_test.txt', False) == 32000000
     assert day20('day20_test2.txt', False) == 11687500
-    assert day20('day20_input.txt', False) == 0
+    assert day20('day20_input.txt', False) == 787056720
+    assert day20('day20_input.txt', True) == 0
