229e2c91350f22f9285e35270ef83bd78616aa14
jackie-h <10471950+jackie-h@users.noreply.github.com>
2023-12-31 12:22:55 +0000

    day 23 remove recursion

--- day23.py
+++ day23.py
@@ -1,3 +1,4 @@
+import collections
 import sys
 
 
@@ -19,7 +20,9 @@ def solve(lines, part2):
     sy = lines[0].find('.')
 
     success = []
-    next_steps(0, sy, lines, [], success, part2)
+    paths = collections.deque()
+    paths.append((0,sy,[]))
+    next_steps(paths, lines, success, part2)
 
     for s in success:
         l = len(s)
@@ -29,50 +32,45 @@ def solve(lines, part2):
     return res
 
 
-def next_steps(x, y, lines, visited, success, part2):
-    if x < 0 or x >= len(lines):
-        return
-    elif y < 0 or y > len(lines[0]):
-        return
+def next_steps(paths, lines, success, part2):
 
-    c = lines[x][y]
+    while len(paths) > 0:
+        x,y,visited = paths.pop()
 
-    if x == len(lines) - 1 and c == '.':
-        success.append(visited.copy())
-        print(len(visited))
+        if 0 <= x < len(lines) and 0 <= y < len(lines[0]):
+            c = lines[x][y]
 
-    elif (x,y) in visited:
-        return
+            if x == len(lines) - 1 and c == '.':
+                success.append(visited.copy())
+                print(len(visited))
+            elif (x,y) not in visited:
+                if c != '#':
+                    visited.append((x,y))
 
-    if c != '#':
-        visited.append((x,y))
+                if part2:
+                    if c != '#':
+                        paths.append((x - 1, y, visited.copy()))
+                        paths.append((x + 1, y, visited.copy()))
+                        paths.append((x, y - 1, visited.copy()))
+                        paths.append((x, y + 1, visited.copy()))
 
-    if part2:
-        if c != '#':
-            next_steps(x - 1, y, lines, visited.copy(), success, part2)
-            next_steps(x + 1, y, lines, visited.copy(), success, part2)
-            next_steps(x, y - 1, lines, visited.copy(), success, part2)
-            next_steps(x, y + 1, lines, visited.copy(), success, part2)
-
-    else:
-        if c == '.':
-            next_steps(x - 1, y, lines, visited.copy(), success, part2)
-            next_steps(x + 1, y, lines, visited.copy(), success, part2)
-            next_steps(x, y - 1, lines, visited.copy(), success, part2)
-            next_steps(x, y + 1, lines, visited.copy(), success, part2)
-        elif c == '<':
-            next_steps(x, y - 1, lines, visited, success, part2)
-        elif c == '>':
-            next_steps(x, y + 1, lines, visited, success, part2)
-        elif c == '^':
-            next_steps(x - 1, y, lines, visited, success, part2)
-        elif c == 'v':
-            next_steps(x + 1, y, lines, visited, success, part2)
+                else:
+                    if c == '.':
+                        paths.append((x - 1, y, visited.copy()))
+                        paths.append((x + 1, y, visited.copy()))
+                        paths.append((x, y - 1, visited.copy()))
+                        paths.append((x, y + 1, visited.copy()))
+                    elif c == '<':
+                        paths.append((x, y - 1, visited))
+                    elif c == '>':
+                        paths.append((x, y + 1, visited))
+                    elif c == '^':
+                        paths.append((x - 1, y, visited))
+                    elif c == 'v':
+                        paths.append((x + 1, y, visited))
 
 
 if __name__ == '__main__':
-    sys.setrecursionlimit(10000)
-
     assert day23('day23_test.txt', False) == 94
     assert day23('day23_input.txt', False) == 2238
 
