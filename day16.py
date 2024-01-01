bdebcaab9edaa7872d9206a9ec1224c2c3aedc27
jackie-h <10471950+jackie-h@users.noreply.github.com>
2024-01-01 14:15:08 +0000

    day 16 fix off-by-one

--- day16.py
+++ day16.py
@@ -35,7 +35,6 @@ def solve(lines, part2):
         ny = location[1] + direction[1]
 
         stop = (location,direction) in visited
-        stop = stop or ((nx == len(lines) - 1) and (ny == len(lines[0]) - 1))
         stop = stop or nx < 0 or nx >= len(lines)
         stop = stop or ny < 0 or ny >= len(lines[0])
 
@@ -76,12 +75,19 @@ def solve(lines, part2):
                     beams.append(((nx, ny), direction))
 
     res = len(locations_visited)
+
+    for x,y in locations_visited:
+        lines[x] = lines[x][0:y] + '#' + lines[x][y+1:]
+
+    for row in lines:
+        print(*row, sep="")
+
     return res
 
 
 
 if __name__ == '__main__':
-    #assert day16('day16_test.txt', False) == 46
-    assert day16('day16_input.txt', False) == 46
+    assert day16('day16_test.txt', False) == 46
+    assert day16('day16_input.txt', False) == 6994
 
 
