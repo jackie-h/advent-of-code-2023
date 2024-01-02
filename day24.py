cdd39e4ceba425e0d3f945203a1adaa1a09e9947
jackie-h <10471950+jackie-h@users.noreply.github.com>
2024-01-02 11:50:17 +0000

    day 24 part1 initial working

--- day24.py
+++ day24.py
@@ -28,27 +28,51 @@ def solve(lines, x_min, x_max):
         p1, v1 = i1
         p2, v2 = i2
         d1 = diff_coords(p1, p2)
-        np1 = move(p1, v1)
-        np2 = move(p2, v2)
+        np1 = move(p1, v1, 1)
+        np2 = move(p2, v2, 1)
         d2 = diff_coords(np1, np2)
         print(p1, p2, d1, np1, np2, d2)
         if d2 < d1:
-            res += 1
+            t1 = max_t_inside(p1, v1, x_min, x_max)
+            t2 = max_t_inside(p2, v2, x_min, x_max)
+            t = min(t1,t2)
+            mp1 = move(p1, v1, t)
+            mp2 = move(p2, v2, t)
+            d3 = diff_coords(mp1, mp2)
+            if d3 > d2:
+                res += 1
+                print('yes=', p1, p2)
 
     # x + t*v = x1 + t*v2
 
     return res
 
+def max_t_inside(p1,v1,min_x,max_x):
+    x,y,z = p1
+    vx,vy,vz = v1
+    t = 0
+    if vx < 0:
+        tx = (x - min_x) / abs(vx)
+    else:
+        tx = (max_x - x) / vx
 
-def move(p1, v1):
-    return p1[0] + v1[0], p1[1] + v1[1], p1[2] + v1[2]
+    if vy < 0:
+        ty = (y - min_x) / abs(vy)
+    else:
+        ty = (max_x - y) / vy
+    t = min(ty,tx)
+    return t
+
+
+def move(p1, v1, t):
+    return p1[0] + t * v1[0], p1[1] + t * v1[1], p1[2] + t * v1[2]
 
 
 def diff_coords(p1, p2):
     diff = (p1[0] - p2[0], p1[1] - p2[1], p1[2] - p2[2])
     #Just x and y for part 1
     total = abs(diff[0]) + abs(diff[1])
-    return (diff, total)
+    return total
 
 
 if __name__ == '__main__':
