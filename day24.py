54485869a24bc4d458750fd99ed88526fc8fe302
jackie-h <10471950+jackie-h@users.noreply.github.com>
2024-01-02 11:04:17 +0000

    day 24 initial

--- day24.py
+++ day24.py
@@ -1,4 +1,4 @@
-from operator import itemgetter
+import itertools
 
 
 def day24(filename, part2):
@@ -18,20 +18,37 @@ def solve(lines, part2):
 
     coords = []
     for line in lines:
-        head,tail = line.split(" @ ")
-        x,y,z = head.split(", ")
-        a,b,c = tail.split(", ")
-        coords.append(((int(x),int(y),int(z)),(int(a),int(b),int(c))))
+        head, tail = line.split(" @ ")
+        x, y, z = head.split(", ")
+        a, b, c = tail.split(", ")
+        coords.append(((int(x), int(y), int(z)), (int(a), int(b), int(c))))
 
-    sorted(coords, key=itemgetter(2))
+    for p in itertools.combinations(coords, 2):
+        i1, i2 = p
+        p1, v1 = i1
+        p2, v2 = i2
+        d1 = diff_coords(p1, p2)
+        np1 = move(p1, v1)
+        np2 = move(p2, v2)
+        d2 = diff_coords(np1, np2)
+        print(p1, p2, d1, np1, np2, d2)
+        if d2 < d1:
+            res += 1
 
- #x + t*v = x1 + t*v2
+    # x + t*v = x1 + t*v2
 
     return res
 
 
+def move(p1, v1):
+    return p1[0] + v1[0], p1[1] + v1[1], p1[2] + v1[2]
+
+
+def diff_coords(p1, p2):
+    diff = (p1[0] - p2[0], p1[1] - p2[1], p1[2] - p2[2])
+    total = abs(diff[0]) + abs(diff[1]) + abs(diff[2])
+    return (diff, total)
+
 
 if __name__ == '__main__':
-    assert day24('day24_test.txt', False) == 5
-
-
+    assert day24('day24_test.txt', False) == 2
