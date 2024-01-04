44f317b01289fa79029b24ffd8ce5bdb4d2ee26c
jackie-h <10471950+jackie-h@users.noreply.github.com>
2024-01-04 17:56:52 +0000

    day 17 part 1

--- day17.py
+++ day17.py
@@ -32,7 +32,7 @@ def solve(lines, min_dist, max_dist):
             directions = next_directions(prev_direction)
             for direction in directions:
                 cost_increase = 0
-                print(direction)
+                #print(direction)
 
                 for distance in range(1, max_dist + 1):
                     nx = x + direction[0] * distance
@@ -45,7 +45,7 @@ def solve(lines, min_dist, max_dist):
                         if costs.get((nx, ny, direction), 1e100) <= nc:
                             continue
                         costs[(nx, ny, direction)] = nc
-                        print(nx,ny,direction,nc)
+                        #print(nx,ny,direction,nc)
                         heappush(q, (nc, (nx, ny), direction))
 
 
@@ -67,3 +67,4 @@ def next_directions(direction):
 
 if __name__ == '__main__':
     assert day17('day17_test.txt', 1, 3) == 102
+    assert day17('day17_input.txt', 1, 3) == 1039
