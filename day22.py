e70f37dc39cd64819b657ccee2368330717b3653
jackie-h <10471950+jackie-h@users.noreply.github.com>
2024-01-08 16:35:19 -0500

    Day 22: fix settle

--- day22.py
+++ day22.py
@@ -1,6 +1,4 @@
 from collections import defaultdict
-from operator import itemgetter
-
 
 def day22(filename, part2):
     print('Day 22: Sand Slabs')
@@ -32,28 +30,28 @@ def solve(lines, part2):
     return res
 
 def settle(coords):
-    bf,bv = 0,[]
+    prev_lk = 1
     new_coords = defaultdict(set)
     for k,v in coords.items():
-        nk = bf + 1
         for block in v:
-            bk = nk
+            new_bz = block[0][2]
             clear = True
-            while clear and bk != 1:
-                tk = bk - 1
+            while clear and new_bz != 1:
+                tk = min(prev_lk, new_bz - 1)
+                new_bz = tk
                 for c in new_coords[tk]:
                     overlap = x_y_overlap(block, c)
                     print('overlap',block,c,overlap)
                     clear = not overlap
                     if not clear:
-                        break
-                if clear:
-                    bk = tk
-            diff = k - bk
-            new_c = ((block[0][0], block[0][1], bk), (block[1][0], block[1][1], block[1][2] - diff))
-            print(block,new_c,bk,diff)
-            new_coords[bk].add(new_c)
-        bf = k
+                        new_bz = max(new_bz,c[1][2] + 1)
+
+            diff = k - new_bz
+            new_c = ((block[0][0], block[0][1],new_bz), (block[1][0], block[1][1], block[1][2] - diff))
+            print(block,new_c,new_bz,diff)
+            new_coords[new_bz].add(new_c)
+        prev_lk = k
+
 
     return new_coords
 
@@ -80,9 +78,10 @@ def can_remove(coords):
                 print('supporting count',block, support_count, support_count == 0 or support_count >= 2)
                 support_counts[block] = support_count
 
-    for k,v in coords.items():
+    keys = list(coords.keys())
+    for k in keys:
         above_k = k + 1
-        for block in v:
+        for block in coords[k]:
             is_supporting = False
             for c in coords[above_k]:
                 overlap = x_y_overlap(block, c)
@@ -106,15 +105,20 @@ def x_y_overlap(block1, block2):
     b2_x2 = block2[1][0]
     x_overlap = b1_x1 in range(b2_x1, b2_x2+1) or b1_x2 in range(b2_x1, b2_x2+1) \
             or b2_x1 in range(b1_x1, b1_x2+1) or b2_x2 in range(b1_x1, b1_x2+1)
+    if not x_overlap:
+        return False
     b1_y1 = block1[0][1]
     b1_y2 = block1[1][1]
     b2_y1 = block2[0][1]
     b2_y2 = block2[1][1]
     y_overlap = b1_y1 in range(b2_y1,b2_y2+1) or b1_y2 in range(b2_y1,b2_y2+1) \
             or b2_y1 in range(b1_y1,b1_y2+1) or b2_y2 in range(b1_y1,b1_y2+1)
+    if not y_overlap:
+        return False
     return x_overlap and y_overlap
 
 
 if __name__ == '__main__':
     assert day22('day22_test.txt', False) == 5
+    assert day22('day22_test2.txt', False) == 1
     #assert day22('day22_input.txt', False) == 5
