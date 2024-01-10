7c53d7afe5067ecbdc1712d596ea42a985029800
jackie-h <10471950+jackie-h@users.noreply.github.com>
2024-01-10 14:47:03 -0500

    day 22 part 2 initial

--- day22.py
+++ day22.py
@@ -1,5 +1,5 @@
-import collections
 from collections import defaultdict
+
 
 def day22(filename, part2):
     print('Day 22: Sand Slabs')
@@ -23,12 +23,12 @@ def solve(lines, part2):
         a, b, c = tail.split(",")
         coords[int(z)].add(((int(x), int(y), int(z)), (int(a), int(b), int(c))))
 
+    new_coords_by_bz, new_coords_by_tz = settle(coords)
 
-    new_coords_by_bz,new_coords_by_tz = settle(coords)
-
-    res = can_remove(new_coords_by_bz,new_coords_by_tz)
+    res = can_remove(new_coords_by_bz, new_coords_by_tz, part2)
 
     return res
+
 
 def settle(coords):
     prev_lk = 1
@@ -44,9 +44,9 @@ def settle(coords):
                 new_bz = tk
                 for c in new_coords_by_tz[tk]:
                     overlap = x_y_overlap(block, c)
-                    print('overlap',block,c,overlap)
+                    print('overlap', block, c, overlap)
                     if overlap:
-                        new_bz = max(new_bz,c[1][2] + 1)
+                        new_bz = max(new_bz, c[1][2] + 1)
                     clear = clear and not overlap
 
             new_c = ((block[0][0], block[0][1],new_bz), (block[1][0], block[1][1], new_bz + (block[1][2] - block[0][2])))
@@ -58,11 +58,12 @@ def settle(coords):
     return new_coords_by_bz, new_coords_by_tz
 
 
-def can_remove(coords_by_bz, coords_by_tz):
+def can_remove(coords_by_bz, coords_by_tz, part2):
     count = 0
-    bf,bv = 0,[]
 
     support_counts = {}
+    supporting = defaultdict(set)
+    supported_by = defaultdict(set)
 
     keys = sorted(list(coords_by_tz.keys()))
     for k in reversed(keys):
@@ -75,12 +76,15 @@ def can_remove(coords_by_bz, coords_by_tz):
                     print('supporting', block, c, overlap)
                     if overlap:
                         support_count += 1
+                        supporting[c].add(block)
+                        supported_by[block].add(c)
                     if support_count >= 2:
                         break
-                print('supporting count',block, support_count, support_count == 0 or support_count >= 2)
+                print('supporting count', block, support_count, support_count == 0 or support_count >= 2)
                 support_counts[block] = support_count
 
     keys = sorted(list(coords_by_bz.keys()))
+    safe_to_disintegrate = set()
     for k in keys:
         for block in coords_by_bz[k]:
             is_supporting = False
@@ -93,11 +97,31 @@ def can_remove(coords_by_bz, coords_by_tz):
                     is_supporting = supports == 1
                 if is_supporting:
                     break
-            print('can remove',block, not is_supporting)
+            print('can remove', block, not is_supporting)
             if not is_supporting:
                 count += 1
+                safe_to_disintegrate.add(block)
 
-    return count
+    if part2:
+        fall_count = 0
+        for blocks in coords_by_bz.values():
+            for block in blocks:
+                if block not in safe_to_disintegrate:
+                    b_fall_set = set()
+                    q = set()
+                    q.update(supporting.get(block))
+                    while q:
+                        n = q.pop()
+                        b_fall_set.add(n)
+                        for c in supporting.get(n, set()):
+                            cs = supported_by.get(c)
+                            if len(cs) < 2 or cs.issubset(b_fall_set):
+                                q.add(c)
+                    fall_count += len(b_fall_set)
+
+        return fall_count
+    else:
+        return count
 
 
 def x_y_overlap(block1, block2):
@@ -105,22 +129,24 @@ def x_y_overlap(block1, block2):
     b1_x2 = block1[1][0]
     b2_x1 = block2[0][0]
     b2_x2 = block2[1][0]
-    x_overlap = b1_x1 in range(b2_x1, b2_x2+1) or b1_x2 in range(b2_x1, b2_x2+1) \
-            or b2_x1 in range(b1_x1, b1_x2+1) or b2_x2 in range(b1_x1, b1_x2+1)
+    x_overlap = b1_x1 in range(b2_x1, b2_x2 + 1) or b1_x2 in range(b2_x1, b2_x2 + 1) \
+                or b2_x1 in range(b1_x1, b1_x2 + 1) or b2_x2 in range(b1_x1, b1_x2 + 1)
     if not x_overlap:
         return False
     b1_y1 = block1[0][1]
     b1_y2 = block1[1][1]
     b2_y1 = block2[0][1]
     b2_y2 = block2[1][1]
-    y_overlap = b1_y1 in range(b2_y1,b2_y2+1) or b1_y2 in range(b2_y1,b2_y2+1) \
-            or b2_y1 in range(b1_y1,b1_y2+1) or b2_y2 in range(b1_y1,b1_y2+1)
+    y_overlap = b1_y1 in range(b2_y1, b2_y2 + 1) or b1_y2 in range(b2_y1, b2_y2 + 1) \
+                or b2_y1 in range(b1_y1, b1_y2 + 1) or b2_y2 in range(b1_y1, b1_y2 + 1)
     if not y_overlap:
         return False
     return x_overlap and y_overlap
 
 
 if __name__ == '__main__':
-    assert day22('day22_test.txt', False) == 5
-    assert day22('day22_test2.txt', False) == 1
-    assert day22('day22_input.txt', False) == 426
+    # assert day22('day22_test.txt', False) == 5
+    # assert day22('day22_test2.txt', False) == 1
+    # assert day22('day22_input.txt', False) == 426
+
+    assert day22('day22_test.txt', True) == 7
